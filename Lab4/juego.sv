016767969a54fa7f9864be5082e296d5539d084a
Armando <armandofg3937@gmail.com>
2022-04-25 11:38:17 -0600

    avance1

---
+++ Lab4/juego.sv
@@ -0,0 +1,15 @@
+module juego(input logic up,
+input logic down,
+input logic right,
+input logic left,
+input logic play,
+input logic reset,
+input logic clk,
+output reg[15:0] PairsCards,
+output reg[15:0] OpenCards,
+output reg[1:0] win,
+output logic timeOut);
+
+
+
+endmodule
